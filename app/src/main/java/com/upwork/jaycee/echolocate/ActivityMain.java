da2a319719d77299a53844f2aa88bc9a98be58f5
Your Name <you@example.com>
2017-08-15 20:14:52 +0100

    Initial commit

---
+++ app/src/main/java/com/upwork/jaycee/echolocate/ActivityMain.java
@@ -0,0 +1,14 @@
+package com.upwork.jaycee.echolocate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
