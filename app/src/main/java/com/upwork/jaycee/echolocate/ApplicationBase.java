9d565d3fa80268e64711dd38ba81865ecef70ff4
jayceelock <jaycee.lock@gmail.com>
2017-08-22 11:14:10 +0100

    Added log emailer, updated support library, ensured that at least one locationprovider gets selected

---
+++ app/src/main/java/com/upwork/jaycee/echolocate/ApplicationBase.java
@@ -0,0 +1,24 @@
+package com.upwork.jaycee.echolocate;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(
+        mailTo = "jaycee.lock@gmail.com", // my email here
+        customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT },
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast_text)
+public class ApplicationBase extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        // The following line triggers the initialization of ACRA
+        super.onCreate();
+        ACRA.init(this);
+    }
+}
